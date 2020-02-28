9445330e20451a4385230d4985a4ceac7d746f3d
SavSanta <remethyst@yahoo.com>
2020-02-28 07:52:00 -0500

    add import of model to api/views.py for debug test

--- citysync/app/api/views.py
+++ citysync/app/api/views.py
@@ -1,16 +1,17 @@
 from flask import request, jsonify, Blueprint
-from app.models import Nonprofit
+from app.models import Nonprofit, NonprofitSchema
+np = NonprofitSchema()
 
 api_blueprint = Blueprint('api', __name__,)
 
 @api_blueprint.route("/api/orgs/", methods=["GET"])
 def get_orgs():
-    all_orgs = Nonprofit.query.order_by(Nonprofit.ein).all()
-    for org in all_orgs:
-        f = open("/tmp/vimp", "w")
-        f.write("Sneaky Deaky ein: {}, {}".format(org.ein, org.id))
-        f.write(org.__dict__)
-    return (org.serialize())
+    all_orgs = Nonprofit.query.order_by(Nonprofit.ein).get(3)
+    #f = open("/tmp/vimp", "w")
+    #f.write("Sneaky Deaky ein: {}, {}".format(all_orgs.ein, all_orgs.id))
+    #f.write("\n {}".format(jsonify(all_orgs)))
+    #f.close()
+    return np.jsonify(all_orgs)
 
 
 @api_blueprint.route("/api/orgs/<int:ein>/", methods=["GET"])
