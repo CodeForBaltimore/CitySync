8d66a6b6fe2f05783c06e9227aa7df177901cbb7
SavSanta <remethyst@yahoo.com>
2020-03-04 14:00:34 -0500

    API route changed to /all
    Changed API routes to /all and modified code to currently return a mock simulation of all Nonprofit objects. There now exists a NonprofitSchema(many=True) in order to retrieve multiple serialized Nonprofit objects and serialize them with JSONify

--- citysync/app/api/views.py
+++ citysync/app/api/views.py
@@ -1,20 +1,34 @@
 from flask import request, jsonify, Blueprint
 from app.models import Nonprofit, NonprofitSchema
-np = NonprofitSchema()
 
 api_blueprint = Blueprint('api', __name__,)
+npschema = NonprofitSchema()
+npschemas = NonprofitSchema(many=True)
 
-@api_blueprint.route("/api/orgs/", methods=["GET"])
+
+@api_blueprint.route("/api/orgs/all", methods=["GET"])
 def get_orgs():
-    all_orgs = Nonprofit.query.order_by(Nonprofit.ein).get(3)
-    #f = open("/tmp/vimp", "w")
-    #f.write("Sneaky Deaky ein: {}, {}".format(all_orgs.ein, all_orgs.id))
-    #f.write("\n {}".format(jsonify(all_orgs)))
-    #f.close()
-    return np.jsonify(all_orgs)
+    all_orgs = Nonprofit.query.all()
+    # paginate logic
+    records = []
+    for x, org in enumerate(all_orgs):
+        records.append(org)
+        if x == 10:
+            break
+    #return npschemas.jsonify(len(all_orgs))
+    return npschemas.jsonify(records)
 
 
-@api_blueprint.route("/api/orgs/<int:ein>/", methods=["GET"])
-def get_org_ein(ein):
-    #for ein in 
-    return render_template("main/about.html")
+
+@api_blueprint.route("/api/orgs/id/<int:id>/", methods=["GET"])
+def get_org_by_id(id):
+    org = Nonprofit.query.get(id)
+    return npschema.jsonify(org)
+
+
+@api_blueprint.route("/api/orgs/ein/<int:ein>/", methods=["GET"])
+def get_org_by_ein(ein):
+    org = Nonprofit.query.filter(Nonprofit.ein == ein).first()
+    return npschema.jsonify(org)
+
+
