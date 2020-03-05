1ab3585cd498e3645ab7c213948884dd1f6de697
SavSanta <remethyst@yahoo.com>
2020-03-05 08:55:02 -0500

    Add API routes for Address returns
    Added to API routes that use getattr to return data from the model for Nonprofit addresses

--- citysync/app/api/views.py
+++ citysync/app/api/views.py
@@ -5,7 +5,6 @@ api_blueprint = Blueprint('api', __name__,)
 npschema = NonprofitSchema()
 npschemas = NonprofitSchema(many=True)
 
-
 @api_blueprint.route("/api/orgs/all", methods=["GET"])
 def get_orgs():
     all_orgs = Nonprofit.query.all()
@@ -15,9 +14,8 @@ def get_orgs():
         records.append(org)
         if x == 10:
             break
-    #return npschemas.jsonify(len(all_orgs))
-    return npschemas.jsonify(records)
 
+    return npschemas.jsonify(records)
 
 
 @api_blueprint.route("/api/orgs/id/<int:id>/", methods=["GET"])
@@ -26,9 +24,25 @@ def get_org_by_id(id):
     return npschema.jsonify(org)
 
 
+@api_blueprint.route("/api/orgs/id/<int:id>/address/", methods=["GET"])
+def get_org_address_by_id(id):
+    org = Nonprofit.query.get(id)
+    only_these_fields = ["id", "ein", "name", "street", "city", "state", "zipcode"]
+    return jsonify(({ attr : getattr(org, attr) for attr in only_these_fields}))
+
+
 @api_blueprint.route("/api/orgs/ein/<int:ein>/", methods=["GET"])
 def get_org_by_ein(ein):
     org = Nonprofit.query.filter(Nonprofit.ein == ein).first()
     return npschema.jsonify(org)
+
+
+@api_blueprint.route("/api/orgs/ein/<int:ein>/address/", methods=["GET"])
+def get_org_address_by_ein(ein):
+    org = Nonprofit.query.filter(Nonprofit.ein == ein).first()
+    only_these_fields = ["id", "ein", "name", "street", "city", "state", "zipcode"]
+    return jsonify(({ attr : getattr(org, attr) for attr in only_these_fields}))
+
+
 
 
